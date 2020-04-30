05baba87952ca6c3a51acc82edac84f95caaea9a
starch <1020023233@qq.com>
2020-04-30 15:06:09 +0800

    自习室现在打开其它应用三秒后才会失败，消息通知功能建设中

--- app/src/main/java/com/example/study/selfstudyActivity.java
+++ app/src/main/java/com/example/study/selfstudyActivity.java
@@ -1,5 +1,8 @@
 package com.example.study;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -7,6 +10,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.provider.Settings;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
@@ -15,12 +19,14 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationManagerCompat;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class selfstudyActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static final int NOTIFICATION_FLAG = 0x00000008;
     private int minute;
     private int second;
     private TextView tv_selfstudy_time;
@@ -53,6 +59,7 @@ public class selfstudyActivity extends AppCompatActivity implements View.OnClick
 
         shared=getSharedPreferences("share",MODE_PRIVATE);
         integral=shared.getInt("integral",0);
+        check();
 
         startTime();
     }
@@ -79,6 +86,7 @@ public class selfstudyActivity extends AppCompatActivity implements View.OnClick
                 if (!isfail){
                     if (!BackgroundUtil.isForeground(mContext,3,mPackageName)){
                         if (cnt<3){
+                            notificationAPI_16p();
                             cnt++;
                         }
                         else {
@@ -215,5 +223,42 @@ public class selfstudyActivity extends AppCompatActivity implements View.OnClick
             return false;
         }
         return super.onKeyDown(keyCode,event);
+    }
+
+    public void notificationAPI_16p() {
+        // 获取NotificationManager管理者对象
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        // 创建一个PendingIntent，和Intent类似，不同的是由于不是马上调用，需要在下拉状态条出发的Activity，所以采用的是PendingIntent,即点击Notification跳转启动到哪个Activity
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, MainActivity.class), 0);
+        // 通过Notification.Builder来创建通知，注意API Level 16之后才支持
+        Notification notificationAPI_16p = new Notification.Builder(this)
+                // 设置状态栏中的小图片，尺寸一般建议在24×24，这个图片同样也是在下拉状态栏中所显示，如果在那里需要更换更大的图片，可以使用setLargeIcon(Bitmap icon)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                // 设置在status bar上显示的提示文字
+                .setTicker("TickerText:" + "您有新短消息，请注意查收！")
+                // 设置在下拉status bar后显示的标题
+                .setContentTitle("这里是标题（API 16+）")
+                // 设置在下拉status bar后显示的内容
+                .setContentText("这里是显示的内容")
+                // 关联PendingIntent
+                .setContentIntent(pendingIntent)
+                // 设置在下拉status bar后显示的数字
+                .setNumber(1)
+                // 需要注意build()是在API level 16及之后增加的，API11可以使用getNotificatin()来替代
+                .build();
+        // FLAG_AUTO_CANCEL表明当通知被用户点击时，通知将被清除。
+        notificationAPI_16p.flags |= Notification.FLAG_AUTO_CANCEL;
+        // 通过通知管理器来发起通知
+        manager.notify(NOTIFICATION_FLAG, notificationAPI_16p);
+    }
+
+    private void check(){
+        NotificationManagerCompat notification = NotificationManagerCompat.from(this);
+        boolean isEnabled = notification.areNotificationsEnabled();
+        if (!isEnabled) {
+            Intent intent = new Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        }
     }
 }
